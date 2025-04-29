98f5afc1e15720c0a933dd859ed62a63cbe52d45
Igor <webdevbiv@gmail.com>
2025-04-29 14:42:57 +0300

    UPD | refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-// import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { addContact, deleteContact } from './redux/contactsSlice';
 import { setNameFilter } from './redux/filtersSlice';
+import { selectFilteredContacts } from './redux/selectors';
 
 const App = () => {
   const contacts = useSelector(state => state.contacts.items);
+  const filteredContacts = useSelector(selectFilteredContacts);
   const userSearch = useSelector(state => state.filters.name);
   const dispatch = useDispatch();
-  // const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
-  // const [userSearch, setUserSearch] = useState('');
 
-  const filteredContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(userSearch.toLowerCase())
-  );
-
-  const handleUserSearch = e => dispatch(setNameFilter(e.target.value));
+  const handleUserSearch = e => {
+    dispatch(setNameFilter(e.target.value));
+  };
 
   const handleNewContact = (values, { resetForm }) => {
     const isDuplicate = contacts.some(
@@ -35,7 +33,6 @@ const App = () => {
 
     const newContact = { ...values, id: nanoid() };
     dispatch(addContact(newContact));
-
     resetForm();
   };
 
